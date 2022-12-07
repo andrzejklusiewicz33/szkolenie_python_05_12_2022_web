418252922f8d7f8f1a26026f3a6f8df1cd511c49
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-12-07 15:27:23 +0100

    zadanie 52

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hello_world')
-def hello_world():  # put application's code here
-    return 'Hello Mapet! Cośtam'
+@app.route('/')
+def index():  # put application's code here
+    return '<h1>Strona główna</h1>'
 
+@app.route('/show_employees')
+def show_employees():
+    return '<h1>Lista pracowników</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True,port=80)
@@ -14,3 +17,4 @@ if __name__ == '__main__':
 #przerwa do 15:07
 
 #51. Uruchom swoją aplikację na porcie 80 w trybie debug
+#52. Dodaj do programu obsługe ekranów "/show_products", "/about", "/tests" i "/" (strona główna)
