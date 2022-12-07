b9f3c88cda21fa2f8ac16070812acba11b482b5f
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-12-07 15:40:37 +0100

    zadanie 53

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():  # put application's code here
-    return '<h1>Strona główna</h1>'
+def index():
+    return render_template("index.html")#'<h1>Strona główna</h1>'
 
 @app.route('/show_employees')
 def show_employees():
     return '<h1>Lista pracowników</h1>'
+
+@app.route('/show_products')
+def show_products():
+    return "<h1>Lista produktów</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1>Strona o programie</h1>"
+
+@app.route('/tests')
+def tests():
+    return "<h1>Strona do testów</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True,port=80)
@@ -18,3 +30,5 @@ if __name__ == '__main__':
 
 #51. Uruchom swoją aplikację na porcie 80 w trybie debug
 #52. Dodaj do programu obsługe ekranów "/show_products", "/about", "/tests" i "/" (strona główna)
+#53. Zadbaj o to by każdy ekran posiadał swój plik html i by był on pokazywany przy wejsciu na adres.
+#    Każdy z tych plików html powinien mieć jakiś napis np. <h1>DUPA</h1>
