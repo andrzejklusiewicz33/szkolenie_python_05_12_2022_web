cd3cf3dc645974f718500b81c3c23f5f562923e2
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-12-07 15:15:44 +0100

    zadanie 51

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/hello_world')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello Mapet! Cośtam'
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,port=80)
+
+#przerwa do 15:07
+
+#51. Uruchom swoją aplikację na porcie 80 w trybie debug
